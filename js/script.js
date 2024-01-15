8cc2aaad7010914424f4db2e8471fa0c7557e022
Olayinka Christianah <olayinchristianah@gmail.com>
2024-01-15 16:25:53 +0100

    push-p

--- js/script.js
+++ js/script.js
@@ -32,25 +32,30 @@
 // }
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault()
     
-        if (Country.value == 'Select a country' || Country.value=='') {
-            Country.style.borderColor = "red";
-            
-        }
-        if (year.value == 'year' || year.value >= '2003') {
-            year.style.borderColor = "red";
-            alert("SORRY, YOU ARE UNDER THE LEGAL AGE, WE CAN NOT WELCOME YOU ON OUR SITE. " )
-        }
-       
+        if (Country.value == 'Select a country'){
+            Country.style.borderColor = "red"; 
+        }else{
+            Country.style.borderColor = "purple"; 
 
-    //     if((year <= 2003) || (year > year))
+        }
+        if (year.value == 'year'){
+            year.style.borderColor = "red";
+        }else{ 
+            year.style.borderColor = "purple";
+        }
+        
+        
+        if(year.value == 2003){
+        alert("SORRY, YOU ARE UNDER THE LEGAL AGE, WE CAN NOT WELCOME YOU ON OUR SITE. ")
+        }
     //     {
     //     alert("Year should be in range 2002 ");
     //     return false;
     //     }
     // return true;
 
-    e.preventDefault()
     
 
 });
